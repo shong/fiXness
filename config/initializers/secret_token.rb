6ebfa04f714cc935d56bbf226b0ab7d95325f6c6
Shawn Hong <sewoom@gmail.com>
2012-04-05 16:22:34 -0700

    blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FiXness::Application.config.secret_token = '44140ac1d14fa045b9b303d2bf6b1c4f40a150286e79ca2022955778521b5370913fe47ab448ca35451c6beaac61a447dcafe385b87838054fe083be978fb56a'
